e3d23c1abb50f30f9d422fd245a49bf9081b2f8b
beelarr <bryonl@me.com>
2017-12-04 15:33:24 -0600

    User defined types

--- app/graphql/types/author_type.rb
+++ app/graphql/types/author_type.rb
@@ -1,11 +1,17 @@
 Types::AuthorType = GraphQL::ObjectType.define do
   name "AuthorType"
 
-  field :first_name, types.String
+  field :first_name, !types.String #bang means non-nullable field
   field :last_name, types.String
   field :birth_year, types.Int
   field :is_alive, types.Boolean
   field :id, types.ID
+
+  field :full_name, types.String do
+    description 'The authors full name'
+    # _ can be used for unused arguments - saves memeory
+    resolve ->(o,_,_) { [o.first_name, o.last_name].compact.join(' ')}
+  end
 
 
 
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,7 @@
 class Author < ApplicationRecord
+
+#   appends first and last name replaced in author_type.rb
+#   def full_name
+#     [self.first_name, self.last_name].compact.join(' ')
+#   end
 end
